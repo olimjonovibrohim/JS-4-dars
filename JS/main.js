afbd71569b172fb7d0a0126961383f32717d351b
olimjonovibrohim <ib0130nik@gmail.com>
2024-08-22 13:22:05 +0500

    JS 4-dars
    Zeromax IT Center

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+//Math in JS
+
+// let round = Math.round(8.6);                     yaxlitlash-1
+// let pow = Math.pow(5,5);                         darajani hisoblash
+// let sqrt = Math.sqrt(225);                       idizini topish
+// let abs = Math.abs(-12.8);                       absolute sonini chiqarish
+// let ceil = Math.ceil(49.1);                      yaxlitlash-2
+// let floor = Math.floor(49.9);                    yaxlitlash-3
+// let min = Math.min(16,300,2010,14,329,1971);     minial son
+// let max = Math.max(16,300,2010,14,329,1971)      maksimal son
+// let trunc = Math.trunc(49.9)                     yaxlitlash-4
+// document.write(trunc);
+
+
+let byTag = document.getElementsByTagName("h1");
+let byId = document.getElementById("idNomi")
+let byClass = document.getElementsByClassName("classNomi")
+let bySelector = document.querySelector(".classNomi")
+let bySelectorAll = document.querySelectorAll(".classNomi")
+let byAll = document.getElementsByTagName("h1").namedItem("idNomi")
+
+console.log(byAll);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS/main1.js
@@ -0,0 +1,12 @@
+let byAll = document.getElementsByTagName("div").namedItem("comback")
+
+byAll.style.backgroundColor = "blueviolet"
+byAll.style.color = "white"
+byAll.style.width = "500px"
+byAll.style.height = "250px"
+byAll.style.border = "15px solid blue"
+byAll.style.textAlign = "center"
+byAll.style.boxSizing = "border-box"
+byAll.style.marginLeft = "500px"
+
+console.log(byAll);
